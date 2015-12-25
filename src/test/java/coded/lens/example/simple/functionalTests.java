d526c56195aba4ba5e8187d28b70805d57d4ef34
Julian Kelsey <julian.b.kelsey@gmail.com>
2015-12-25 09:51:21 +0000

    simple example of infinite list with javaslang

--- src/test/java/coded/lens/example/simple/functionalTests.java
+++ src/test/java/coded/lens/example/simple/functionalTests.java
@@ -1,7 +1,25 @@
 package coded.lens.example.simple;
 
-/**
- * Created by jbk on 17/12/2015.
- */
+
+import javaslang.collection.Stream;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class functionalTests {
+
+    private Stream<BigInteger> fibs(final BigInteger a, final BigInteger b) {
+        return Stream.cons(a, () -> fibs(b, a.add(b)));
+    }
+
+    @Test
+    public void testName() throws Exception {
+        Stream<BigInteger> fibs = fibs(BigInteger.ZERO, BigInteger.ONE);
+        
+        for (BigInteger l : fibs.drop(1000000).take(3)) {
+            System.out.println(l);
+        }
+
+    }
 }
